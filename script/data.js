fed1cdd2d7bcc8b8ca66f3bdb10f21a8d6e29223
onkar-kalsannawar <onkarok0273@gmail.com>
2022-09-28 21:25:09 +0530

    add data

---
+++ script/data.js
@@ -0,0 +1,103 @@
+// images array
+let img1 = ["https://cdn.shopify.com/s/files/1/0361/8553/8692/products/vitc.jpg?v=1663393830","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Copyofcicanightgel_900x.jpg?v=1663393073","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Copyoflipplumping_900x.jpg?v=1663392224","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/image_6483441_4_900x.jpg?v=1663398401"]
+let img2 = ["https://cdn.shopify.com/s/files/1/0361/8553/8692/products/2_18_900x.jpg?v=1663393830","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/3-CICA-NIGHT-GEL_360x.jpg?v=1663393073","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/4-Lip-Plumpin-Mask_b23993c9-a240-4c02-ba5f-e769c33a5f0c_360x.jpg?v=1663392224","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/3-Probiotics-Gel_360x.jpg?v=1663392956"]
+let img3 = ["https://cdn.shopify.com/s/files/1/0361/8553/8692/products/3_21_900x.jpg?v=1663393830","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/6-CICA-NIGHT-GEL_360x.jpg?v=1663393073","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/5-Lip-Plumpin-Mask_47c8041c-839b-4d4c-8663-4dcafeda13ae_360x.jpg?v=1663392224","https://cdn.shopify.com/s/files/1/0361/8553/8692/products/6-Probiotics-Gel_360x.jpg?v=1663392956"]
+
+// array of product posters
+let poster1 = ["https://i.ibb.co/M6sHds9/poster1-1.png","https://i.ibb.co/yVFkh1H/poster1-2.png","https://i.ibb.co/6ZYkdhZ/poster1-3.png","https://i.ibb.co/Zg6gmmq/poster1-4.png"]
+let poster2 = ["https://cdn.shopify.com/s/files/1/0361/8553/8692/files/Group_413_2.png?v=1661946820","https://i.ibb.co/YWSGymN/poster2-2.png","https://cdn.shopify.com/s/files/1/0361/8553/8692/files/image_267.png?v=1655064323","https://i.ibb.co/88yFRDj/poster2-4.png"]
+let poster3 = ["https://cdn.shopify.com/s/files/1/0361/8553/8692/files/image_371.png?v=1660645343","https://cdn.shopify.com/s/files/1/0361/8553/8692/files/image_373.png?v=1660645343","https://cdn.shopify.com/s/files/1/0361/8553/8692/files/image_367.png?v=1660645343","https://cdn.shopify.com/s/files/1/0361/8553/8692/files/image_369.png?v=1660645343"]
+
+// array of slider images
+let slider1 = ["https://i.ibb.co/Jmtk0wr/slider1-1.png","https://i.ibb.co/6mPbKzt/slider1-2.png","https://i.ibb.co/nD85SnR/slider1-3.png","https://i.ibb.co/RHF5RqP/slider1-4.png"]
+let slider2 = ["https://i.ibb.co/7YLC0t4/slider2-1.png","https://i.ibb.co/tqGnZZH/slider2-2.png","https://i.ibb.co/bXRVt8L/slider2-3.png","https://i.ibb.co/vwBdHMB/slider2-4.png"]
+let slider3 = ["https://i.ibb.co/Njf6MqG/slider3-1.png","https://i.ibb.co/tqh7hd0/slider3-2.png","https://i.ibb.co/k1VD2NR/slider3-3.png","https://i.ibb.co/vd2Vr1h/slider3-4.png"]
+
+// array product name
+let product_name = ["10% Vitamin C + E & 5% Niacinamide Serum for Even-Toned, Glowing Skin | Treats Pigmentation and Dark Spots I Beginner Friendly","CICA Niacinamide Night Gel With Green Tea, Tea Tree Oil & Hyaluronic| Reduces Acne , Dark Spots & Pigmentation | Oily, Acne Prone & Sensitive Skin","Vitamin C + E Lip Sleeping Mask With Shea Butter, Hyaluronic & Almond Oil| Treats Dry, Pigmented Dark Lips| Cruelty Free","72 HR Hydrating Probiotic Gel Moisturizer With Hyaluronic & Japanese Rice Water| For Healthy, Hydrated Skin| Normal to Oily Skin"]
+
+// array of old price and new price
+let price_old = ["695","495","445","420"]
+let price_new = ["625","470","422","395"]
+
+// array of product benifites
+let benifets1 = ["Daily glow enhancing face serum powered by Vitamin C + E.","Dot & Key Cica Niacinamide Night Gel also contains Green Tea, Tea Tree and Hyaluronic","Lightens dark lips and fades pigmentation for naturally pink lips","72 Hr Probiotics Gel Moisturizer with Hyaluronic and Rice water "]
+let benifets2 = ["With 10% Triple Vitamin C to fade hyperpigmentation for even-toned skin","Lightweight night gel for sensitive, oily, acne-prone skin","Moisturizers flaky, dry, chapped lips","Strengthens skin’s barrier to retain moisture"]
+let benifets3 = ["Re-energizes skin, and boosts collagen with Superfruit Sicilian Blood Orange for firm, glowing skin.","Clinically proven to reduce acne, pigmentation and dark spot","Deeply nourishes for plump, plush, supple lips","Balances skin's microbiome and improves overall skin texture"]
+let benifets4 = ["Treat dark spots & acne scars with 5% Niacinamide.","Calms and soothes redness & irritation due to acne","Reverses the effects of sun damage on lips","Oil-free, lightweight, quick-absorbing gel moisturizer"]
+let benifets5 = ["Vitamin E works in harmony with Vitamin C to boost efficacy.","With no added synthetic fragrance","Enhances collagen production and promotes cell regeneration","Skin appears plump, hydrated and dewy"]
+
+
+
+
+// constructore function to create object
+function constructor(img1,img2,img3,poster1,poster2,poster3,slider1,slider2,slider3,product_name,price_old,price_new,benifets1,benifets2,benifets3,benifets4,benifets5){
+    
+    this.img1 = img1,
+    this.img2 = img2,
+    this.img3 = img3,
+
+    this.poster1 = poster1,
+    this.poster2 = poster2,
+    this.poster3 = poster3,
+    
+
+    this.slider1 = slider1,
+    this.slider2 = slider2,
+    this.slider3 = slider3,
+    
+    this.product_name = product_name,
+
+    this.price_old =price_old,
+    this.price_new =price_new,
+
+
+    this.benifets1 = benifets1,
+    this.benifets2 = benifets2,
+    this.benifets3 = benifets3,
+    this.benifets4 = benifets4,
+    this.benifets5 = benifets5
+
+
+}
+
+let N = img1.length
+
+
+let data = []
+
+// push all product object in arr
+
+for(let i=0;i<N;i++){
+    let obj = new constructor(img1[i],img2[i],img3[i],poster1[i],poster2[i],poster3[i],slider1[i],slider2[i],slider3[i],product_name[i],price_old[i],price_new[i],benifets1[i],benifets2[i],benifets3[i],benifets4[i],benifets5[i])
+
+    data.push(obj)
+
+}
+
+console.log(data)
+
+const append = ()=>{
+let cont = document.querySelector("#grid")
+    data.map(({img1,img2,img3,poster1,poster2,poster3,slider1,slider2,slider3,product_name,price_old,price_new,benifets1,benifets2,benifets3,benifets4,benifets5})=>{
+
+        let div = document.createElement("div")
+
+        let image = document.createElement("img")
+        image.src = img1
+
+        
+        
+
+
+        div.append()
+
+        cont.append(div)
+        
+    })
+    
+}
+append()
+
+
+
