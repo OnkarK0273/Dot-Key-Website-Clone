12a68754c67bd53f0b6b4ff19c4773ef3790a3d2
ttushar2622 <tthorat22@gmail.com>
2022-10-01 23:04:42 +0530

    footer

--- cart.js
+++ cart.js
@@ -2,6 +2,17 @@ import { navbar } from "./components/navbar.js";
 let navbar_div=document.getElementById("navbar");
 
 navbar_div.innerHTML = navbar();
+let btn=document.getElementById("theButton");
+btn.addEventListener("click",function(){
+  clicked()
+  // console.log("hii")
+})
+
+function clicked() {
+  var text = document.getElementById("popup");
+  text.classList.toggle("hide");
+  text.classList.toggle("show");
+}
 
 let arr = JSON.parse(localStorage.getItem("addtocard")) || []
 
